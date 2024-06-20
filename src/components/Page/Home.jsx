7c6230a144a6c1e1cdf11ce4cbe7aa1982b5fb75
Maryam Saba <maryams91101@gmail.com>
2024-06-21 00:15:03 +0500

    axios

--- src/components/Page/Home.jsx
+++ src/components/Page/Home.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Task from '../Tasks/Task.jsx'
+import axios from 'axios';
 //import AddTaskList from '../AddTasks/AddTaskList.jsx'
 
 
 
 const Home = () => {
+  const [todoList , setTodoList] = useState([]);
+
+  async function getTodos ()  {
+   try {
+    const response = await axios.get("http://localhost:3000/AllTodos");
+    console.log(response);
+   } catch (error) {
+    
+   }
+  }
   return (
     <div className='flex justify-center gap-7'>
       <Task/>
